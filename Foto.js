fe885e8d447e3caa5031243c4c7393b61f0719a9
diego-sousa-st <diegosousa.st@gmail.com>
2017-08-14 19:42:10 -0300

    merge interface com algoritmo

---
+++ Foto.js
@@ -0,0 +1,6 @@
+var Foto = function(instructionStatus,reservationStation,registerFile,clock){
+		this.instructionStatus = instructionStatus;
+		this.reservationStation = reservationStation;
+		this.registerFile = registerFile;
+		this.clock = clock;
+};
